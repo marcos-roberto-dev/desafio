97eb74829e90ad88c8adb6a702ad29bb19b976ce
marksfrontdev <marcosprojetosdev@gmail.com>
2019-11-01 16:27:54 -0300

    Component de íntegra noticias

---
+++ src/views/NewsInFull.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1>{{fetchData}}</h1>
+</template>
+
+<script>
+import { API } from "@/services/api";
+
+export default {
+  name: "NewsInFull",
+  data() {
+    return {
+      fetchData: {}
+    };
+  },
+  props: ["filter"],
+  methods: {
+    async newsInFull() {
+      const { data } = await API.get("/noticias.json");
+      const select = data.find(news => news.date === this.filter);
+      this.fetchData = { ...select };
+    }
+  },
+  created() {
+    this.newsInFull();
+  }
+};
+</script>
+
+<style>
+</style>
